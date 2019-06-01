7c856bd39eac74e4e33fcd76bcbb89fe4655818c
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-06-01 19:34:54 +0200

    Created a new interface with Xcode

---
+++ FasterThanLightFourierTransform/FasterThanLightFourierTransform/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FasterThanLightFourierTransform
+//
+//  Created by Giorgia Adorni on 01/06/2019.
+//  Copyright © 2019 Giorgia e Elia. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FasterThanLightFourierTransform/FasterThanLightFourierTransform/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  FasterThanLightFourierTransform
+//
+//  Created by Giorgia Adorni on 01/06/2019.
+//  Copyright © 2019 Giorgia e Elia. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func userDidDropImage(_ sender: NSImageView) {
+    }
+    
+    @IBAction func openButtonPressed(_ sender: NSButton) {
+        let panel = NSOpenPanel()
+        
+        panel.allowedFileTypes = [kUTTypeImage as String]
+        
+        panel.directoryURL = URL(fileURLWithPath: NSHomeDirectory())
+            .appendingPathComponent("FtlFT/images", isDirectory: true)
+        
+        panel.beginSheetModal(for: self.view.window!) { response in
+            if response == .OK {
+                
+            }
+        }
+    }
+    
+}
+
