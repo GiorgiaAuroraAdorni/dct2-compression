f6ddb235f0343f1c1d2e4eeabeec841bfdff2ab5
Elia Cereda <eliacereda@gmail.com>
2019-06-11 00:05:24 +0200

    Cleaned up the code, configured Python bridging to load custom code
    Python sources must be added to the src/ folder, which will be copied inside the compiled .app bundle.

--- FasterThanLightFourierTransform/FasterThanLightFourierTransform/AppDelegate.swift
+++ FasterThanLightFourierTransform/FasterThanLightFourierTransform/AppDelegate.swift
@@ -11,6 +11,17 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    override init() {
+        // Force the bridging code to load Python 3
+        PythonLibrary.useVersion(3)
+        
+        let sys = Python.import("sys")
+        let pythonSources = Bundle.main.path(forResource: "src", ofType: nil)
+        
+        // Append the src/ folder to PYTHONPATH to be able to import it
+        sys.path.append(pythonSources)
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
--- FasterThanLightFourierTransform/FasterThanLightFourierTransform/ViewController.swift
+++ FasterThanLightFourierTransform/FasterThanLightFourierTransform/ViewController.swift
@@ -16,43 +16,25 @@ class ViewController: NSViewController {
     @IBOutlet weak var windowSlider: PreciseSliderView!
     @IBOutlet weak var cutOffSlider: PreciseSliderView!
     
+    private let py = Python.import("main")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        // Python:
-        //    import numpy as np
-        //    a = np.arange(15).reshape(3, 5)
-        //    b = np.array([6, 7, 8])
-        let np = Python.import("numpy")
-        let a = np.arange(15).reshape(3, 5)
-        let b = np.array([6, 7, 8])
-        
-        //    // Python:
-        //    //    import gzip as gzip
-        //    //    import pickle as pickle
-        //    let gzip = Python.import("gzip")
-        //    let pickle = Python.import("pickle")
-        //
-        //    // Python:
-        //    //    file = gzip.open("mnist.pkl.gz", "rb")
-        //    //    (images, labels) = pickle.load(file)
-        //    //    print(images.shape) // (50000, 784)
-        //    let file = gzip.open("mnist.pkl.gz", "rb")
-        //    let (images, labels) = pickle.load(file).tuple2
-        //    print(images.shape) // (50000, 784)
-        //
     }
 
     @IBAction func userDidDropImage(_ sender: NSImageView) {
-        DispatchQueue.main.async {
-            let a = sender.image!.makeNumpyArray()
-            
-            let plt = Python.import("matplotlib.pyplot")
-            
-            plt.imshow(a)
-            plt.show()
-        }
+        py.main()
+        
+//        DispatchQueue.main.async {
+//            let a = sender.image!.makeNumpyArray()
+//            
+//            let plt = Python.import("matplotlib.pyplot")
+//            
+//            plt.imshow(a)
+//            plt.show()
+//        }
     }
     
     @IBAction func open(_ sender: NSButton) {
