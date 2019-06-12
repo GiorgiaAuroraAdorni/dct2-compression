932e5522c1013badbee6ef22a7e7d1d5ac263267
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-06-12 23:53:14 +0200

    Added test

--- FasterThanLightFourierTransform/FasterThanLightFourierTransform/src/example.py
+++ FasterThanLightFourierTransform/FasterThanLightFourierTransform/src/example.py
@@ -39,3 +39,24 @@ plt.subplot(1, 2, 2)
 plt.imshow(final, cmap='gray', vmin=0, vmax=255)
 
 plt.show()
+
+################## TEST ##################
+
+prova_blocco = np.array([[231,    32,   233,   161,    24,    71,   140,   245],
+                            [247,    40,   248,   245,   124,   204,    36,   107],
+                            [234,   202,   245,   167,     9,   217,   239,   173],
+                            [193,   190,   100,   167,    43,   180,     8,    70],
+                            [ 11,    24,   210,   177,    81,   243,     8,   112],
+                            [ 97,   195,   203,    47,   125,   114,   165,   181],
+                            [193,    70,   174,   167,    41,    30,   127,   245],
+                            [ 87,   149,    57,   192,    65,   129,   178,   228]])
+
+
+
+prova_c = dctn(prova_blocco, type=2, norm='ortho')
+
+prova_compressed = compression(prova_c)
+
+prova_ff = idctn(prova_compressed, type=2, norm='ortho')
+
+prova_normalized_ff = normalize(prova_ff)
