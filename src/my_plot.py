51b3f8d19c3bf05886b5e41c8f107f4acf965e32
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-06-15 16:56:54 +0200

    Test: added lines O(n) O(n^2) O(n^3)

--- src/my_plot.py
+++ src/my_plot.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def comparison_result(x, y_set, labels, title, filename):
 
     for i in range(len(y_set)):
         plt.plot(x, y_set[i], 'o-', linewidth=3, markersize=8, label=labels[i])
+
+    y = x * np.log(x) / 100000
+    y_square = (x ** 2) * np.log(x) / 100000000
+    y_cube = (x ** 3) * np.log(x) / 100000000000
+
+    plt.plot(x, y, '--', linewidth=3, markersize=8, label="n")
+    plt.plot(x, y_square, '--', linewidth=3, markersize=8, label="n^2")
+    plt.plot(x, y_cube, '--', linewidth=3, markersize=8, label="n^3")
 
     plt.yscale('log')
 
